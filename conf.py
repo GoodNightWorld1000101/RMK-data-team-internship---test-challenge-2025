13166fc27e3479747d9d889ac308ba51a4c06238
Christopher Robin Subbi <robinsubbi@outlook.com>
2025-05-20 01:16:15 +0300

    fixed filter_data_function so that it only considers busses that run within the weekdays

--- conf.py
+++ conf.py
@@ -5,6 +5,7 @@ STOPS_PATH = DATA_FOLDER_PATH +"/stops.txt"
 STOP_TIMES_PATH = DATA_FOLDER_PATH +"/stop_times.txt"
 TRIPS_PATH = DATA_FOLDER_PATH +"/trips.txt"
 ROUTES_PATH = DATA_FOLDER_PATH +"/routes.txt"
+CALENDAR_PATH = DATA_FOLDER_PATH +"/calendar.txt" 
 
 
 
